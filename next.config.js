863309148f5c0359ea15b8eea74daf67c224bc68
Gabriel Rapucci <gabrielrapuccigonzalez14@hotmail.com>
2024-04-10 09:41:34 -0300

    Add and configure PWA with next-pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require("next-pwa")({
+    dest: "public",
+    disable: process.env.NODE_ENV !== "production"
+})
+
+const nextConfig = withPWA({
     reactStrictMode: true,
     output: process.env.BUILD_STANDALONE === "true" ? "standalone" : undefined
-}
+})
 
 module.exports = {
     ...nextConfig,

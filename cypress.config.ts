235a96e67d01e81c5917c680c9fe2ffddfdef7f8
Gabriel Rapucci <gabrielrapuccigonzalez14@hotmail.com>
2024-05-07 00:11:50 -0300

    Create a CI pipeline with Github Actions

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "cypress"
 
 export default defineConfig({
+    projectId: "wuwagt",
     e2e: {
-        baseUrl: "https://tailwind.gabrielrapucci.com.br",
-        viewportWidth: 1366,
-        viewportHeight: 768
+        baseUrl: "http://localhost:3000"
     }
 })
